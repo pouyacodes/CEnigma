6edd0db198490384b1961d0ab66cd0b7297ea7e2
Pouya Rezvani <thesirpr@gmail.com>
2024-02-16 06:25:25 +0330

    da_list.h: Dynamic array
    Adding more convenient macros and fixing some stupid bugs!

--- include/da_list.h
+++ include/da_list.h
@@ -1,33 +1,42 @@
 #ifndef _X_DA_LIST
 #define _X_DA_LIST
 
-#ifndef DA_INIT_CAP
+#include <stddef.h>
+
+#ifndef DA_INIT_CA
 #define DA_INIT_CAP 2
 #endif
 
 #ifndef DA_REALLOC
-#define DA_REALLOC(ptr, size) realloc(ptr, size)
+#    include <stdlib.h>
+#    define DA_REALLOC(ptr, size) realloc(ptr, size)
 #endif
 
 #ifndef DA_FREE
-#define DA_FREE(ptr) free(ptr)
+#    include <stdlib.h>
+#    define DA_FREE(ptr) free(ptr)
 #endif
 
 #ifndef DA_ASSERT
-#define DA_ASSERT(a) assert(a)
+#    include <assert.h>
+#    define DA_ASSERT(a) assert(a)
 #endif
 
 #define da_append(da, item) do { \
-        assert((da) != NULL); \
-        if ((da)->cap >= (da)->len) { \
+        DA_ASSERT((da) != NULL); \
+        if ((da)->cap == (da)->len) { \
             (da)->cap = (da)->cap == 0 ? DA_INIT_CAP : (da)->cap * 2;\
             (da)->items = DA_REALLOC((da)->items, sizeof((da)->items[0]) * sizeof((da)->cap));\
-            assert((da)->items != NULL);\
+            DA_ASSERT((da)->items != NULL);\
         } \
         (da)->items[(da)->len++] = (item); \
     } while (0)
 
 #define da_free(da) DA_FREE((da)->items)
+
+#define da_foreach(da) for (size_t i = 0; i < (da)->len; ++i)
+
+#define da_rev_foreach(da) for (ssize_t i = (da)->len - 1; i >= 0; --i)
 
 #define DYNAMIC_ARRAY(t) \
     size_t cap;\
